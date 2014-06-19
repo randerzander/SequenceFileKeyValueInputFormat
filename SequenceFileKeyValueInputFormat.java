437057bfb8367fd73db3e661a5cb9a9a04e68c34
randerzander <rgelhau@gmail.com>
2014-06-19 08:30:33 -0400

    initial commit

---
+++ SequenceFileKeyValueInputFormat.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.MapFile;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hadoop.mapred.InputSplit;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RecordReader;
+import org.apache.hadoop.mapred.Reporter;
+
+public class SequenceFileKeyValueInputFormat<K, V> extends FileInputFormat<K, V> {
+  public SequenceFileKeyValueInputFormat() {
+    setMinSplitSize(SequenceFile.SYNC_INTERVAL);
+  }
+  
+  @Override
+  protected FileStatus[] listStatus(JobConf job) throws IOException {
+    FileStatus[] files = super.listStatus(job);
+    for (int i = 0; i < files.length; i++) {
+      FileStatus file = files[i];
+      if (file.isDir()) {     // it's a MapFile
+        Path dataFile = new Path(file.getPath(), MapFile.DATA_FILE_NAME);
+        FileSystem fs = file.getPath().getFileSystem(job);
+        // use the data file
+        files[i] = fs.getFileStatus(dataFile);
+      }
+    }
+    return files;
+  }
+
+  public RecordReader<K, V> getRecordReader(InputSplit split, JobConf job, Reporter reporter)
+    throws IOException {
+    reporter.setStatus(split.toString());
+    return (RecordReader<K, V>) new SequenceFileKeyValueRecordReader<K, V>(job, (FileSplit) split);
+  }
+}
---
+++ SequenceFileKeyValueRecordReader.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hadoop.mapred.RecordReader;
+import org.apache.hadoop.util.ReflectionUtils;
+
+public class SequenceFileKeyValueRecordReader<K, V> implements RecordReader<K, V> {
+  private SequenceFile.Reader in;
+  private long start;
+  private long end;
+  private boolean more = true;
+  protected Configuration conf;
+
+  public SequenceFileKeyValueRecordReader(Configuration conf, FileSplit split) throws IOException {
+    Path path = split.getPath();
+    FileSystem fs = path.getFileSystem(conf);
+    this.in = new SequenceFile.Reader(fs, path, conf);
+    this.end = split.getStart() + split.getLength();
+    this.conf = conf;
+
+    if (split.getStart() > in.getPosition()) in.sync(split.getStart());
+    this.start = in.getPosition();
+    more = start < end;
+  }
+
+  @Override
+  public K createKey() { return (K) ReflectionUtils.newInstance(in.getKeyClass(), conf); }
+  @Override
+  public V createValue(){ return (V) ReflectionUtils.newInstance(in.getValueClass(), conf); }
+  @Override
+  public float getProgress() throws IOException {
+    if (end == start) return 0.0f;
+    else return Math.min(1.0f, (in.getPosition() - start) / (float)(end - start));
+  }
+
+  public synchronized long getPos() throws IOException { return in.getPosition(); }
+  protected synchronized void seek(long pos) throws IOException { in.seek(pos); }
+  public synchronized void close() throws IOException { in.close(); }
+
+  @Override
+  public boolean next(K key, V value) throws IOException {
+    if (!more) return false;
+    long pos = in.getPosition();
+    boolean remaining = in.next((Writable) key, (Writable) value);
+    
+    if (remaining) ((Text)value).set(key.toString() + "\001" + value.toString()); 
+    if (pos >= end && in.syncSeen()) more = false;
+    else more = remaining;
+    return more;
+  }
+}
